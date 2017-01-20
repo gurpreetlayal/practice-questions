40857532063f69895b1b3c36945d6e3376cc16ba
gurpreet-fds <gurpreet@formationds.com>
2017-01-19 21:42:00 -0800

    added comments to DP solutions

--- allQuestionsBook.cpp
+++ allQuestionsBook.cpp
@@ -40,7 +40,7 @@ public:
 };
 
 
-****
+***************************************************************************************************
 
 LC 240. Search a 2D Matrix II
 Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
@@ -57,7 +57,7 @@ checking if val < M[r][c] if so col-- otherwise row++;
 
 
 
-****
+***************************************************************************************************
 
 LC 54. Spiral Matrix
 Given a matrix of m x n elements (m rows, n columns), return all elements of the matrix in spiral order.
@@ -97,7 +97,7 @@ vector<int> spiralOrder(vector<vector<int>>& m) {
 
 
 
-****
+***************************************************************************************************
 
 LC 378. Kth Smallest Element in a Sorted Matrix
 Given a n x n matrix where each of the rows and columns are sorted in ascending order, find the kth smallest element in the matrix.
@@ -138,7 +138,7 @@ public:
 };
 
 
-****
+***************************************************************************************************
 
 
 
@@ -184,7 +184,425 @@ public:
 ***************************************************************************************************
 
 
+198. House Robber
 
+You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them is that adjacent houses have security system connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
+
+Given a list of non-negative integers representing the amount of money of each house, determine the maximum amount of money you can rob tonight without alerting the police.
+
+
+
+MAIN IDEA:
+At any i, choose either to pick or not pick vi. Take the max out of it.
+
+DP EQUATION:
+
+DP[i] = max(DP[i-1], v[i] + DP[i-2])
+
+
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        vector<int> cache(nums.size()+1, 0);
+        
+        int max_i_2 = 0;
+        for (int i = 1; i <= nums.size(); i++) {
+            cache[i] = max(nums[i-1] + max_i_2, cache[i-1]);
+            if (max_i_2 < cache[i-1]) {
+                max_i_2 = cache[i-1];
+            }
+        }
+        return cache[nums.size()];
+    }
+};
+
+***************************************************************************************************
+
+
+53. Maximum Subarray
+
+Find the contiguous subarray within an array (containing at least one number) which has the largest sum.
+
+For example, given the array [-2,1,-3,4,-1,2,1,-5,4],
+the contiguous subarray [4,-1,2,1] has the largest sum = 6.
+
+
+2 Solutions. 1 with DP, 2nd using kadane's algorithm
+
+DP Solution
+===========
+MAIN IDEA: At every i, pick max(num[i], max seen until i-1, max which includes i-1 value as well + num[i])
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        if (nums.empty()) {
+            return -1;
+        }
+        int m_at_i = nums[0];
+        int m_till_i = nums[0];
+        
+        for (int i = 1; i < nums.size(); i++) {
+            m_at_i = max(nums[i], m_at_i + nums[i]);
+            m_till_i = max(m_at_i, m_till_i);
+        }
+        
+        return m_till_i;
+    }
+};
+
+
+Kadane Algorithm
+================
+MAIN IDEA: starting from i = 0, keep on adding numbers. If the sum becomes negative, reject that whole set.
+Also, everytime you add a num[i] maintain a global max. Return global max at the end.
+
+
+int maxSubArray(int* a, int n) {
+  if (a == NULL || n <= 0) {
+      return 0;
+  }
+  
+  int s = 0, e = 0;
+  return kadane(a, n, &s, &e);
+}
+
+int kadane(int *a, int n, int* s, int* e) {
+    int cm = a[0], cms = 0, cme = 0;
+    int m = a[0];
+    
+    for (int i = 1; i < n; i++) {
+        if (cm < 0) {
+            cm = 0;
+            cms = i;
+        }
+        
+        cm += a[i];
+        cme = i;
+        
+        if (cm > m) {
+            m = cm;
+            *s = cms;
+            *e = cme;
+        }
+    }
+    return m;
+}
+
+
+***************************************************************************************************
+
+338. Counting Bits
+
+Given a non negative integer number num. For every numbers i in the range 0 ≤ i ≤ num calculate the number of 1's in their binary representation and return them as an array.
+
+Example:
+For num = 5 you should return [0,1,1,2,1,2].
+
+Follow up:
+
+It is very easy to come up with a solution with run time O(n*sizeof(integer)). But can you do it in linear time O(n) /possibly in a single pass?
+Space complexity should be O(n).
+Can you do it like a boss? Do it without using any builtin function like __builtin_popcount in c++ or in any other language.
+
+
+MAIN IDEA:
+For every number,
+if number is even then it will have same number of 1's in its binary representation as has number/2
+if odd then it will have 1 + number of bits in number/2
+
+
+class Solution {
+public:
+    vector<int> countBits(int num) {
+        if (num <= 0) {
+            return vector<int>(1,0);
+        }
+        vector<int> cache(num+1);
+        cache[0] = 0;
+        cache[1] = 1;
+        for (int i = 2; i <= num; i++) {
+            if (i%2) {
+                cache[i] = 1 + cache[i/2];
+            } else {
+                cache[i] = cache[i/2];
+            }
+        }
+        
+        return cache;
+    }
+};
+
+
+***************************************************************************************************
+
+139. Word Break
+
+Given a non-empty string s and a dictionary wordDict containing a list of non-empty words, determine if s can be segmented into a space-separated sequence of one or more dictionary words. You may assume the dictionary does not contain duplicate words.
+
+For example, given
+s = "leetcode",
+dict = ["leet", "code"].
+
+Return true because "leetcode" can be segmented as "leet code".
+
+
+MAIN IDEA:
+Basically look for all substrings ending at i starting from 0->i-1. See if they are in dictionary, if they are then the result at i will be equal to result that was at cache[j].
+
+DP[i] = DP[j] for all j: 0 ->i-1 if (substr(j,i) is in dictionary)
+
+class Solution {
+public:
+    bool wordBreak(string s, unordered_set<string>& dict) {
+        vector<bool> cache(s.size() + 1, false);
+        cache[0] = true;
+        
+        for (int i = 1; i <= s.size(); i++) {
+            for (int j = 1; j <= i; j++) {
+                auto subst = s.substr(j - 1, i - j + 1);
+                if (dict.find(subst) != dict.end()) {
+                    cache[i] = cache[j - 1];
+                }
+                if (cache[i]) {
+                    break;
+                }
+            }
+        }
+        return cache[s.size()];
+    }
+};
+
+
+
+***************************************************************************************************
+
+
+152. Maximum Product Subarray
+
+Find the contiguous subarray within an array (containing at least one number) which has the largest product.
+
+For example, given the array [2,3,-2,4],
+the contiguous subarray [2,3] has the largest product = 6.
+
+
+MAIN IDEA:
+Since the numbers can be negative as well and -num2*-num1 is positive, so at every i we calculate
+max as well as min, since this min could become max if next i is -ve. So at every i we calculate
+max according to dp equation below:
+
+DP[i] = max (max_at_i-1*num[i], min_at_i-1*num[i], num[i])
+and keep a global_max, keep it updated with every inspection of number i:0->n-1
+
+
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+       
+        
+        int max_at_i_minus_1 = nums[0];
+        int max_till_i_minus_1 = nums[0];
+        int min_at_i_minus_1 = nums[0];
+        
+        for (int i = 1; i < nums.size(); i++) {
+           auto max_at_i = max(max(max_at_i_minus_1*nums[i],min_at_i_minus_1*nums[i]), nums[i]);
+           auto min_at_i = min(min(max_at_i_minus_1*nums[i],min_at_i_minus_1*nums[i]), nums[i]);
+           if (max_at_i > max_till_i_minus_1) max_till_i_minus_1 = max_at_i;
+           max_at_i_minus_1 = max_at_i;
+           min_at_i_minus_1 = min_at_i;
+        }
+        return max_till_i_minus_1;
+    }
+    
+    void print(vector<int> v) {
+        for (auto& c : v) {
+            cout << c << " ";
+        }
+        cout <<endl;
+    }
+};
+
+
+***************************************************************************************************
+
+96. Unique Binary Search Trees
+
+Given n, how many structurally unique BST's (binary search trees) that store values 1...n?
+
+For example,
+Given n = 3, there are a total of 5 unique BST's.
+
+   1         3     3      2      1
+    \       /     /      / \      \
+     3     2     1      1   3      2
+    /     /       \                 \
+   2     1         2                 3
+
+
+MAIN IDEA: For a given n, we can choose left subtree varying from (0 -> n) where are right subtree varying from (n -> 0).
+Total subtrees will then be leftsubtreecount*rightsubtreecount.
+
+class Solution {
+public:
+    int uniqueTreeDP(int n, int* cache) {
+    
+        if (cache[n] > 0) {
+            return cache[n];
+        }
+        if (n <= 2) {
+            cache[n] = n;
+            return cache[n];
+        }
+    
+        int t = 0;
+        for (int i = 0; i < n; i++) {
+            auto getLeft = uniqueTreeDP(i, cache);
+            auto getRight = uniqueTreeDP(n - 1 - i, cache);
+            (getLeft == 0) ? getLeft = 1: getLeft;
+            (getRight == 0) ? getRight = 1: getRight;
+    
+            t += getLeft * getRight;
+        }
+        cache[n] = t;
+    
+        return cache[n];
+    }
+    
+    int numTrees(int n) {
+        int cache[n + 1];
+        for (int i = 0; i <= n; i++) cache[i] = 0;
+    
+        return uniqueTreeDP(n, cache);
+    }
+};
+
+
+***************************************************************************************************
+
+62. Unique Paths
+
+A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+
+The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+
+How many possible unique paths are there?
+
+
+MAIN IDEA : 
+DP[i][j] = DP[i-1][j] + DP[i][j-1]
+
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        int** cache = new int*[m];
+        for (int i = 0; i < m; i++) {
+            cache[i] = new int[n];
+        }
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++)
+                cache[i][j] = 0;
+        return uniquePathsCore(cache, m-1, n-1);
+    }
+    
+    int uniquePathsCore(int** cache, int m, int n) {
+        if (m < 0 || n < 0) {
+            return 0;
+        }
+        
+        if (cache[m][n] > 0) {
+            return cache[m][n];
+        }
+        
+        if (m == 0 && n == 0) {
+            cache[m][n] = 1;
+            return cache[m][n];
+        }
+        
+        cache[m][n] = (uniquePathsCore(cache, m-1, n) + uniquePathsCore(cache, m, n-1));
+        return cache[m][n];
+    }
+};
+
+
+***************************************************************************************************
+
+64. Minimum Path Sum
+
+Given a m x n grid filled with non-negative numbers, find a path from top left to bottom right which minimizes the sum of all numbers along its path.
+
+Note: You can only move either down or right at any point in time.
+
+
+MAIN IDEA : 
+DP[i][j] = val[i][j] + min (DP[i-1][j],DP[i][j-1])
+
+
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        if (grid.empty()) {
+            return 0;
+        }
+        int row = grid.size();
+        int col = grid[0].size();
+        vector<vector<int>> cache(row+1, vector<int>(col+1, INT_MAX));
+        
+        for (int i = 1; i <= row; i++) {
+            for (int j = 1; j <= col; j++) {
+                auto minval = min(cache[i-1][j], cache[i][j-1]);
+                (minval == INT_MAX) ? minval = 0 : minval;
+                cache[i][j] = grid[i-1][j-1] + minval;
+            }
+        }
+        return cache[row][col];
+    }
+};
+
+***************************************************************************************************
+
+322. Coin Change
+
+You are given coins of different denominations and a total amount of money amount. Write a function to compute the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
+
+Example 1:
+coins = [1, 2, 5], amount = 11
+return 3 (11 = 5 + 5 + 1)
+
+Example 2:
+coins = [2], amount = 3
+return -1.
+
+Note:
+You may assume that you have an infinite number of each kind of coin.
+
+
+MAIN IDEA: Pseudo polynomial time, it's a subset sum problem with repetitions or a knapsack problem with unlimited number of all items.
+run time O(n.|S|) where n is number of items and |S| is length of input, here it's the amount.
+
+DP[target] = min(1+DP[target-vi]) for all i numbers v[0->n-1]
+
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int> cache(amount+1, amount+1);
+        
+        cache[0] = 0;
+        for (int t = 1; t <= amount; t++) {
+            for (int num = 0; num < coins.size(); num++) {
+                if (coins[num] <= t) {
+                    cache[t] = min(cache[t], 1+cache[t-coins[num]]);
+                }
+            }
+        }
+        
+        if (cache[amount] > amount) { return -1; } else return cache[amount];
+    }
+    
+};
+
+
+
+***************************************************************************************************
 
 23. Merge k Sorted Lists
 Merge k sorted linked lists and return it as one sorted list. Analyze and describe its complexity.
